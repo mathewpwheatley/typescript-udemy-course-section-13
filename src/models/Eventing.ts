829e149fdbee11e82bd5804dd5c81227ff91b8d3
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-10-01 20:44:26 -0700

    User refactor into eventing, sync, attributes

---
+++ src/models/Eventing.ts
@@ -0,0 +1,25 @@
+// Type alias for a function
+type Callback = () => void
+
+export class Eventing {
+  events: { [key: string]: Callback[] } = {}
+
+  on(eventName: string, callback: Callback) {
+    const handlers = this.events[eventName] || []
+    handlers.push(callback)
+    this.events[eventName] = handlers
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName]
+
+    // Check there are handlers, if so run them all
+    if (!handlers || handlers.length === 0) {
+      return
+    } else {
+      handlers.forEach((callback) => {
+        callback()
+      })
+    }
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,64 +1,17 @@
-import axios, { AxiosResponse } from 'axios'
+import { Eventing } from './Eventing'
+import { Sync } from './Sync'
 
 // Type interface for user properties
-interface UserProps {
+export interface UserProps {
   id?: number
   name?: string
   age?: number
 }
 
-// Type alias for a function
-type Callback = () => void
+const rootUrl = 'http://localhost:3000/users'
 
 // User class
 export class User {
-  events: { [key: string]: Callback[] } = {}
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName]
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update)
-  }
-
-  on(eventName: string, callback: Callback) {
-    const handlers = this.events[eventName] || []
-    handlers.push(callback)
-    this.events[eventName] = handlers
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName]
-
-    // Check there are handlers, if so run them all
-    if (!handlers || handlers.length === 0) {
-      return
-    } else {
-      handlers.forEach((callback) => {
-        callback()
-      })
-    }
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data)
-      })
-  }
-
-  save(): void {
-    const id = this.get('id')
-    if (id) {
-      // put
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-    } else {
-      // post
-      axios.post('http://localhost:3000/users', this.data)
-    }
-  }
+  public events: Eventing = new Eventing()
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
 }
