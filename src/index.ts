658f7c28af530b94a1aec9e9883f2825a8f108a4
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-10-01 19:25:16 -0700

    Implements get and post request in User

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,4 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Bob', age: 20 })
 
-user.on('change', () => {
-  console.log('change 1')
-})
-
-user.on('change', () => {
-  console.log('change 2')
-})
-
-user.on('save', () => {
-  console.log('save 1')
-})
-
-user.trigger('save')
+user.save()
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,8 @@
+import axios, { AxiosResponse } from 'axios'
+
 // Type interface for user properties
 interface UserProps {
+  id?: number
   name?: string
   age?: number
 }
@@ -37,6 +40,25 @@ export class User {
       handlers.forEach((callback) => {
         callback()
       })
+    }
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data)
+      })
+  }
+
+  save(): void {
+    const id = this.get('id')
+    if (id) {
+      // put
+      axios.put(`http://localhost:3000/users/${id}`, this.data)
+    } else {
+      // post
+      axios.post('http://localhost:3000/users', this.data)
     }
   }
 }
